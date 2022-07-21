0129016d9b6484452ed6ea7c63e03e888f8e269f
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-21 12:03:56 +0200

    Initial Commit

---
+++ DownloadSaveWithFMandCache/DownloadSaveWithFMandCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadSaveWithFMandCacheApp.swift
+//  DownloadSaveWithFMandCache
+//
+//  Created by Skorobogatow, Christian on 21/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloadSaveWithFMandCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
